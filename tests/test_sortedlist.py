6a84e251c488168944230b894af9d40b47165494
Nick Baliesnyi <nikita.balesny@gmail.com>
2021-01-19 18:15:52 +0200

    add tests for monobound

--- tests/test_sortedlist.py
+++ tests/test_sortedlist.py
@@ -2,7 +2,7 @@
 
 import pytest
 from random import randint
-from witchcraft.sortedlist import SplitList
+from witchcraft.sortedlist import SplitList, MonoboundSplitList
 
 __author__ = "brurucy"
 __copyright__ = "brurucy"
@@ -26,6 +26,29 @@ def test_split_list_not_in():
 
 def test_split_list_many():
     list_instance = SplitList()
+    random_integers = [randint(0, 2000000) for i in range(100000)]
+    for i in random_integers:
+        list_instance.insert(i)
+    for i in random_integers:
+        assert list_instance.lookup(i)
+
+def test_monobound_split_list_in():
+    list_instance = MonoboundSplitList()
+    list_instance.insert(1)
+    list_instance.insert(2)
+    assert list_instance.lookup(1)
+    assert list_instance.lookup(2)
+
+
+def test_monobound_split_list_not_in():
+    list_instance = MonoboundSplitList()
+    list_instance.insert(1)
+    list_instance.insert(2)
+    assert not list_instance.lookup(3)
+
+
+def test_monobound_split_list_many():
+    list_instance = MonoboundSplitList()
     random_integers = [randint(0, 2000000) for i in range(100000)]
     for i in random_integers:
         list_instance.insert(i)
